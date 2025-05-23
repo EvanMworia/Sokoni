54146ff6b1b2357c5d27155c6cc3201c234a9c83
EvanMworia <evannji99@gmail.com>
2025-05-23 23:48:51 +0300

    Chore: Added server.js as the entry point

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+// server.js
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { usersRoutes } from './routes/usersRoutes';
+// 1. Load environment variables
+dotenv.config();
+
+// 2. Initialize app
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // for parsing JSON requests
+
+app.use('/users', usersRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
