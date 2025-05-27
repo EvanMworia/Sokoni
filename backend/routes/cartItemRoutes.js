fa555657ed023b635c6978654a464fcc325c0906
EvanMworia <evannji99@gmail.com>
2025-05-27 23:17:46 +0300

    Chore: Protected Cart Routes

---
+++ backend/routes/cartItemRoutes.js
@@ -0,0 +1,12 @@
+import ex from 'express';
+import { addItemToCart, clearCart, deleteCartItem, getCartItems } from '../controllers/cartContoller.js';
+import { authenticateToken } from '../middlewares/authenticate.js';
+
+const cartItemRoutes = ex.Router();
+
+cartItemRoutes.post('/upsert', addItemToCart);
+cartItemRoutes.get('/items/', authenticateToken, getCartItems);
+cartItemRoutes.delete('/delete-item/:id', authenticateToken, deleteCartItem);
+cartItemRoutes.delete('/clear-cart/', authenticateToken, clearCart);
+
+export default cartItemRoutes;
