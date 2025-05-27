9722b32b166d5c20a51ae78410d4fd1a59d32b03
EvanMworia <evannji99@gmail.com>
2025-05-27 23:22:44 +0300

    Feat: Added a middleware logger to debug request as they come

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRoutes from './routes/usersRoutes.js';
 import productsRoutes from './routes/productsRoutes.js';
+import cartItemRoutes from './routes/cartItemRoutes.js';
 // 1. Load environment variables
 dotenv.config();
 
@@ -12,9 +13,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json()); // for parsing JSON requests
+app.use((req, res, next) => {
+	console.log(`[${req.method}] ${req.url}`);
+	console.log('Incoming body:', req.body);
+	next();
+});
 
 app.use('/users', usersRoutes);
 app.use('/products', productsRoutes);
+app.use('/cartItem', cartItemRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
