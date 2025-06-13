9174bec385933715b7733c2d25fa01a26a3e38f1
EvanMworia <evannji99@gmail.com>
2025-06-13 16:45:28 +0300

    Feat: Stripe now supports cart items which is an array and computes the total

--- backend/controllers/stripeController.js
+++ backend/controllers/stripeController.js
@@ -11,22 +11,27 @@ const stripeInit = new Stripe(process.env.SK_TEST);
 export async function checkoutSession(req, res) {
 	try {
 		const { cartItems } = req.body;
+		if (!cartItems || cartItems.length === 0) {
+			return res.status(404).json({ message: 'Cart is empty' });
+		}
 
-		const lineItems = cartItems.map((item) => {
+		const lineItems = cartItems.map((item) => ({
 			price_data: {
-			}
-		});
-		const checkoutSession = stripeInit.checkout.sessions.create({
-			success_url: 'https://example.com/success',
-			cancel_url: '',
-			line_items: [
-				{
-					price: 'price_1MotwRLkdIwHu7ixYcPLm5uZ',
-					quantity: 2,
+				currency: 'kes',
+				product_data: {
+					name: item.Name,
 				},
-			],
+				unit_amount: item.Price * 100,
+			},
+			quantity: item.Quantity,
+		}));
+		const checkoutSession = await stripeInit.checkout.sessions.create({
+			success_url: 'https://example.com/success',
+			//cancel_url: '',
+			line_items: lineItems,
 			mode: 'payment',
 		});
+		return res.status(200).json({ url: checkoutSession.url });
 	} catch (error) {
 		console.error('Something failed in checkout session', error);
 		return res.status(500).json({ message: 'Something went wrong with Stripe' });
